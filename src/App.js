0fd054bcca7414f1c5b39452e0cc30d89f111b12
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-06-28 04:05:34 -0700

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
+import { PDFExport, savePDF } from "@progress/kendo-react-pdf";
 import './App.css';
 
 function App() {
+  const pdfExportComponent = React.useRef(null);
+
+  const exportPDFWithMethod = () => {
+    let element = document.querySelector(".k-grid") || document.body;
+    savePDF(element, {
+      paperSize: "A4",
+    });
+  };
+
+  const exportPDFWithComponent = () => {
+    if (pdfExportComponent.current) {
+      pdfExportComponent.current.save();
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+       <div>
+      <div className="example-config">
+        <button className="k-button" onClick={exportPDFWithComponent}>
+          Export to PDF with component
+        </button>
+        &nbsp;
+        <button className="k-button" onClick={exportPDFWithMethod}>
+          Export to PDF with method
+        </button>
+      </div>
+
+      <PDFExport ref={pdfExportComponent} paperSize="A4">
+        <table>
+          <tr>
+            <th>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+            </th>
+          </tr>
+        </table>
+      </PDFExport>
     </div>
+    </>
   );
 }
 
